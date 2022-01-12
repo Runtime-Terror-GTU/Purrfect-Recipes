1326c1754d75253e485aca4c31d80e963aa66241
yesimyalc <y.yalcin072@gmail.com>
2022-01-12 19:29:46 +0300

    Add Recipe Functions Started

--- android/app/src/main/java/com/example/purrfectrecipes/EditRecipeFragment.kt
+++ android/app/src/main/java/com/example/purrfectrecipes/EditRecipeFragment.kt
@@ -16,6 +16,10 @@ import com.example.purrfectrecipes.Adapters.ChosenIngredientsRVAdapter
 import com.example.purrfectrecipes.Adapters.RecipeNewStepsRVAdapter
 import com.example.purrfectrecipes.Adapters.RecipeStepsRVAdapter
 import com.example.purrfectrecipes.Customer.AddedrecipesProfileViewModel
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+
 
 class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe) {
 
@@ -24,6 +28,8 @@ class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe) {
     private var recipeTagsRVAdapter: ChosenIngredientsRVAdapter?=null
     private var recipeIngredientsRVAdapter: ChosenIngredientsRVAdapter?=null
     private var recipeStepsRVAdapter: RecipeNewStepsRVAdapter?=null
+
+    val PICK_IMAGE = 1
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -37,6 +43,12 @@ class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe) {
         val recipePic=view.findViewById<ImageView>(R.id.recipePic)
         val recipeName=view.findViewById<EditText>(R.id.recipeName)
         val recipeIngredientDetails=view.findViewById<EditText>(R.id.recipeIngredientDetails)
+
+        viewModel.getRecipeSteps().observe(viewLifecycleOwner,{
+                recipeStepsRVAdapter?.setSteps(viewModel.getRecipeSteps().value!!)
+                recipeStepsRVAdapter?.notifyDataSetChanged()
+        })
+
         viewModel.getRecipe().observe(viewLifecycleOwner,{
             if(viewModel.getRecipe().value!=null)
             {
@@ -79,14 +91,50 @@ class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe) {
                 Toast.makeText(requireContext(), "Please enter the step information first.", Toast.LENGTH_SHORT).show()
             else
             {
+                viewModel.addStep(newStep.text.toString())
                 newStep.clearFocus()
                 newStep.setText("")
             }
         }
 
+        val changeRecipePicButton=view.findViewById<TextView>(R.id.changeRecipePic)
+        changeRecipePicButton.setOnClickListener {
+            val intent = Intent()
+            intent.type = "image/*"
+            intent.action = Intent.ACTION_GET_CONTENT
+            startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE)
+        }
+
+        val addTag=view.findViewById<Button>(R.id.addTag)
+        addTag.setOnClickListener {
+            
+        }
+        val addIngredient=view.findViewById<Button>(R.id.addIngredient)
+        addIngredient.setOnClickListener {
+
+        }
+
         val cancelEditButton=view.findViewById<ImageView>(R.id.cancelEdit)
         cancelEditButton.setOnClickListener {
             addedRecipesViewModel.setEditRecipe(false)
+        }
+
+        val editRecipeButton=view.findViewById<LinearLayout>(R.id.editRecipe)
+        editRecipeButton.setOnClickListener {
+            if(recipeName.text.isNullOrEmpty())
+                Toast.makeText(requireContext(), "Please enter recipe name first.", Toast.LENGTH_SHORT).show()
+            else if(recipeIngredientDetails.text.isNullOrEmpty())
+                Toast.makeText(requireContext(), "Please enter recipe ingredient details first.", Toast.LENGTH_SHORT).show()
+            else if(viewModel.getRecipe().value!=null && viewModel.getRecipeIngredients().value!!.isEmpty())
+                Toast.makeText(requireContext(), "Please add recipe ingredients first.", Toast.LENGTH_SHORT).show()
+            else if(viewModel.getRecipe().value!=null && viewModel.getRecipeTags().value!!.isEmpty())
+                Toast.makeText(requireContext(), "Please add recipe tags first.", Toast.LENGTH_SHORT).show()
+            else if(viewModel.getRecipe().value!=null && viewModel.getRecipeSteps().value!!.isEmpty())
+                Toast.makeText(requireContext(), "Please add recipe steps first.", Toast.LENGTH_SHORT).show()
+            else
+            {
+
+            }
         }
     }
 
@@ -106,5 +154,13 @@ class EditRecipeFragment : Fragment(R.layout.fragment_edit_recipe) {
         recipeStepsRV?.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         recipeStepsRVAdapter = RecipeNewStepsRVAdapter(requireContext())
         recipeStepsRV?.adapter = recipeStepsRVAdapter
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val recipePic=view?.findViewById<ImageView>(R.id.recipePic)
+        if (requestCode == PICK_IMAGE) {
+            val selectedImageUri: Uri? = data?.getData()
+            recipePic?.setImageURI(selectedImageUri)
+        }
     }
 }
--- android/app/src/main/java/com/example/purrfectrecipes/EditRecipeViewModel.kt
+++ android/app/src/main/java/com/example/purrfectrecipes/EditRecipeViewModel.kt
@@ -11,9 +11,36 @@ class EditRecipeViewModel: ViewModel()
     private var recipe= MutableLiveData<Recipe?>()
         fun getRecipe(): LiveData<Recipe?> {return recipe}
 
+    private var recipeTags=MutableLiveData<ArrayList<String>>()
+        fun getRecipeTags():LiveData<ArrayList<String>>{return recipeTags}
+    private var recipeIngredients=MutableLiveData<ArrayList<String>>()
+        fun getRecipeIngredients():LiveData<ArrayList<String>>{return recipeIngredients}
+    private var recipeSteps=MutableLiveData<ArrayList<String>>()
+        fun getRecipeSteps():LiveData<ArrayList<String>>{return recipeSteps}
+
+    init{
+        recipeTags.value= ArrayList()
+        recipeIngredients.value= ArrayList()
+        recipeSteps.value= ArrayList()
+    }
+
     fun setRecipe(retrievedRecipe:Recipe?)
     {
         recipe.value=retrievedRecipe
+    }
+
+    fun addStep(stepText:String)
+    {
+        if(recipe.value!=null)
+        {
+            recipe.value!!.addStage(stepText)
+            recipe.value=recipe.value
+        }
+        else
+        {
+            recipeSteps.value!!.add(stepText)
+            recipeSteps.value=recipeSteps.value
+        }
     }
 
 
