b8d3e9fea1eacc23d91b3095746f15b0ef463aa3
essavran <eminesultansavran@gmail.com>
2022-01-17 20:21:03 +0300

    admin&mod pages are created

---
+++ web/src/pages/admin.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import ScrollToTop from '../components/ScrollToTop';
+import AdminPage from '../components/AdminPage';
+
+const Admin = () => {
+    return (
+        <>
+            <ScrollToTop />
+            <AdminPage />
+        </>
+    )
+}
+
+export default Admin;
---
+++ web/src/pages/moderator.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from 'react';
+import ScrollToTop from '../components/ScrollToTop';
+import ModeratorPage from '../components/ModeratorPage';
+
+const Moderator = () => {
+    return (
+        <>
+            <ScrollToTop />
+            <ModeratorPage />
+        </>
+    )
+}
+
+export default Moderator;
