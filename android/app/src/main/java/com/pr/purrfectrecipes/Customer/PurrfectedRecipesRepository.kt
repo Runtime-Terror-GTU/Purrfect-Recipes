97c9070c8199b6c8b9d1ba66b8b93973af60fbea
yesimyalc <y.yalcin072@gmail.com>
2022-01-19 22:45:21 +0300

    Bug Fix

--- android/app/src/main/java/com/pr/purrfectrecipes/Customer/PurrfectedRecipesRepository.kt
+++ android/app/src/main/java/com/pr/purrfectrecipes/Customer/PurrfectedRecipesRepository.kt
@@ -86,6 +86,8 @@ class PurrfectedRecipesRepository(val connector: RecipesRetrievedListener)
             override fun onDataChange(addedR: DataSnapshot) {
                 val recipesArray=ArrayList<Recipe>()
                 var i=0
+                if(addedR.childrenCount.toInt()==0)
+                    connector.onRecipesRetrieved(recipesArray)
                 for(ds in addedR.children)
                 {
                     recipesRef.child(ds.key.toString()).addListenerForSingleValueEvent(object :
